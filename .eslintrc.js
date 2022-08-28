974bc0d283556a8397abf519078e352460ab89dd
Dorian Alvarado <alvaradogdorian@gmail.com>
2022-08-28 03:27:56 -0300

    chore: [#1] ESLint, Prettier & Husky
    * Install lint dependencies and tools to enforce code quality
    * Add config files for: eslint, prettier, husky & lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    // Import Rules
+    'import/no-cycle': 2,
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type'
+        ],
+        'newlines-between': 'always'
+      }
+    ]
+  }
+};
---
+++ lint-staged.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'src/**/*.{js,jsx,ts,tsx}': [
+    () => 'npm run typecheck:staged',
+    'npm run lint:staged',
+    'npm run test:staged',
+    'npm run prettier:staged'
+  ]
+};
