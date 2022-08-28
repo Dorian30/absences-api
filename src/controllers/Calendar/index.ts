34bfd1a484b930552237871d8027b9ae2515476b
Dorian Alvarado <alvaradogdorian@gmail.com>
2022-08-28 19:45:54 -0300

    feat: [#4] Calendar endpoint
    * Set findAbsences to return all absences when no page param is sent
    * Add calendar service
    * Add calendar controller and set it in container

---
+++ src/controllers/Calendar/index.ts
@@ -0,0 +1,21 @@
+import { Response, Request } from 'express';
+
+import { CalendarService } from '../../services';
+
+export class GetCalendarController {
+  private calendarService: CalendarService;
+
+  constructor(
+    readonly opts: {
+      calendarService: CalendarService;
+    }
+  ) {
+    this.calendarService = opts.calendarService;
+  }
+
+  async executeImpl(req: Request, res: Response) {
+    const calendar = await this.calendarService.createCalendar(req);
+
+    return res.send(calendar);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request } from 'express';
 
-import { GetAbsencesController } from './controllers';
+import { GetAbsencesController, GetCalendarController } from './controllers';
 import container from './container';
 
 const app = express();
@@ -9,6 +9,12 @@ const port = process.env.PORT || 3000;
 app.get('/absences', (req: Request<any>, res) =>
   container
     .resolve<GetAbsencesController>('getAbsencesController')
+    .executeImpl(req, res)
+);
+
+app.get('/calendar', (req: Request<any>, res) =>
+  container
+    .resolve<GetCalendarController>('getCalendarController')
     .executeImpl(req, res)
 );
 
